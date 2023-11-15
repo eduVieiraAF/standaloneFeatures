95256656275e4b44416ebe13512c52354bb72874
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-15 15:28:09 -0300

    components + initial configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HeaderComponent } from './core/components/header/header.component';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -5,12 +6,14 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   template: `
+    <app-header></app-header>
     <router-outlet></router-outlet>
   `,
   standalone: true,
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    HeaderComponent
   ],
 
 })
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 // import { AppModule } from './app/app.module'
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { importProvidersFrom } from '@angular/core';
 
 
 bootstrapApplication(AppComponent, {
-  providers: [],
+  providers: [
+    importProvidersFrom(AppRoutingModule)
+  ],
 })
   .catch(err => console.error(err));
