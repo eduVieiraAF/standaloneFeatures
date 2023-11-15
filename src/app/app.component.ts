17794d4ef6386377eccf3cc20b67b60bffdb4b20
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-15 15:10:31 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  template: `
+    <router-outlet></router-outlet>
+  `,
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+
 })
 export class AppComponent {
   title = 'standaloneFeatures';
