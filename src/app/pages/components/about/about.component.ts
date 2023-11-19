58be666d28d706f8b3b86ef311f72a1fc99a5e73
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-18 22:55:44 -0300

    typed reactive forms

--- src/app/pages/components/about/about.component.ts
+++ src/app/pages/components/about/about.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.sass']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  public form = this.formBuilder.group({
+    email: ['edu@edu.net'],
+    password: ['craze']
+  });
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+  ngOnInit(): void {
+    console.log(this.form.value);
+  }
 
 }
