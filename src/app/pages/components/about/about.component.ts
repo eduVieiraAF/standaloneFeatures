aacf7a17e12e224407ccd1472c5a447f872f790c
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-18 23:04:50 -0300

    inject()

--- src/app/pages/components/about/about.component.ts
+++ src/app/pages/components/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { getRouteParams } from 'src/app/helpers/get-routes-params';
 
 @Component({
   selector: 'app-about',
@@ -15,11 +16,14 @@ export class AboutComponent implements OnInit {
     password: ['craze']
   });
 
+  public id = getRouteParams('id');
+
   constructor(
     private formBuilder: FormBuilder
   ) {}
   ngOnInit(): void {
     console.log(this.form.value);
+    console.log(this.id);
   }
 
 }
